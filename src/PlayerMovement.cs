cc412b06d9632a46732662454dee5b19e10553ea
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-04-23 09:25:39 +0200

    Update PlayerMovement.cs
    Schaden wurde hinzugefügt und wenn leben gleich 0 ist wird die Szene neu geladen

--- src/PlayerMovement.cs
+++ src/PlayerMovement.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public int health = 100;
+
     public float speed = 5;
     public float jump = 5;
 
@@ -18,10 +21,12 @@ public class PlayerMovement : MonoBehaviour
 
     private bool loop = true;
 
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Awake()
@@ -69,9 +74,45 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+           
+                StartCoroutine(blinkRed());
+            health -= 10;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
+            Debug.Log("Player Health: " + health);
+          
 
+            if (health <= 0)
+            {
+
+                Die();
+
+              
+                
+            }
+        }
+    }
+
+    private IEnumerator blinkRed()
+    { 
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+
+
+    private void Die()
+{
+    UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    Debug.Log("Player is dead!");
+}
 
 }
+
+
 
 
 
