6f34d4cd2c25a43655b582ad222df333d045aa2a
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-06-12 17:40:01 +0200

    Update medikit.cs

--- src/medikit.cs
+++ src/medikit.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Medikit : MonoBehaviour
 {
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-
             PlayerMovement player = collision.GetComponent<PlayerMovement>();
-            player.health += 20; 
+            player.health += 20;
+            player.PlayMedikitSound();
             Destroy(gameObject);
         }
     }
