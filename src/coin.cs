b21e676c1b74c098c50357c7917c5473123d8123
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-06-12 22:12:47 +0200

    ICollectible Interface mit Coin und Medikit implementiert

--- src/coin.cs
+++ src/coin.cs
@@ -2,31 +2,30 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using TMPro;
-
-public class Coin : MonoBehaviour
+public class Coin : MonoBehaviour, ICollectible
 {
     public AudioClip coinClip;
     private AudioSource audioSource;
-
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             PlayerMovement player = collision.GetComponent<PlayerMovement>();
-            player.coins++;
+            OnCollect(player);
             player.PlayCoinSound();
             Destroy(gameObject);
         }
     }
-
-    private void PlaySound(AudioClip audioClip)
+    public void OnCollect(PlayerMovement player)
     {
-        audioSource.clip = audioClip;
-        audioSource.Play();
+        player.coins += GetWert();
+    }
+    public int GetWert()
+    {
+        return 1;
     }
 }
--- src/medikit.cs
+++ src/medikit.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 
-public class Medikit : MonoBehaviour
+public class Medikit : MonoBehaviour, ICollectible
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             PlayerMovement player = collision.GetComponent<PlayerMovement>();
-            player.Heal(20);
+            OnCollect(player);
             player.PlayMedikitSound();
             Destroy(gameObject);
         }
+    }
+
+    public void OnCollect(PlayerMovement player)
+    {
+        player.Heal(GetWert());
+    }
+
+    public int GetWert()
+    {
+        return 20;
     }
 }
