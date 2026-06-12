f4225b06fe46e2ff2b35e0f39005de39bd832a5f
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-06-12 17:52:17 +0200

    Update coin.cs

--- src/coin.cs
+++ src/coin.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
-   
+    public AudioClip coinClip;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-
             PlayerMovement player = collision.GetComponent<PlayerMovement>();
             player.coins++;
+            player.PlayCoinSound();
             Destroy(gameObject);
         }
+    }
+
+    private void PlaySound(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 }
